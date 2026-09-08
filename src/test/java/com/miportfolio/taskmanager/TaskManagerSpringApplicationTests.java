9e72fbdb2012de26cd29f17a0f4619bf3dcec99a
Tonyvirues <tonimavir@gmail.com>
2026-09-08 12:38:36 +0200

    Initial commit: proyecto base spring Boot generado con spring Initializr

---
+++ src/test/java/com/miportfolio/taskmanager/TaskManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miportfolio.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
